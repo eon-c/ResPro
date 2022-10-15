7cb20a73c3e3e237f56932ebf5d925d6168d7c02
eon-c <eoncole999@gmail.com>
2022-10-15 12:35:48 +0530

    Testing integration

--- script.js
+++ script.js
@@ -1,33 +1,30 @@
-console.log("no error");
+// console.log("no error");
 switcher = 0;
 function testit() {
-	document.getElementById('test').innerHTML += "<p> CHanged liKe a pRO </p> <p> new line? </p>";
+  document.getElementById("test").innerHTML +=
+    "<p> CHanged liKe a pRO </p> <p> new line? </p>";
 }
 
 caVal = [];
 
-
 function cardFocus(num) {
-	cards = document.getElementsByClassName('card');
-	if (switcher) {
-		var i;
-		for(i=0; i < cards.length; i++)
-		{
-			cards[i].style.display='inline-block';
-		}
-		cards[num].style.height='32vh';
-		cards[num].style.width='32%';
-		switcher ^=1;
-	}
-	else {
-		var i;
-		for(i=0; i < cards.length; i++)
-		{
-			cards[i].style.display='none';
-		}
-		cards[num].style.display='inline-block';
-		cards[num].style.height='70vh';
-		cards[num].style.width='60%';
-		switcher ^=1;
-	}
+  cards = document.getElementsByClassName("card");
+  if (switcher) {
+    var i;
+    for (i = 0; i < cards.length; i++) {
+      cards[i].style.display = "inline-block";
+    }
+    cards[num].style.height = "32vh";
+    cards[num].style.width = "32%";
+    switcher ^= 1;
+  } else {
+    var i;
+    for (i = 0; i < cards.length; i++) {
+      cards[i].style.display = "none";
+    }
+    cards[num].style.display = "inline-block";
+    cards[num].style.height = "70vh";
+    cards[num].style.width = "60%";
+    switcher ^= 1;
+  }
 }
